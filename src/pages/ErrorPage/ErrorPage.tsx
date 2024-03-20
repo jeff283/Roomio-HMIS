36c99b930907c2297a0c64a226f9521367800401
Jeff <you@example.com>
2024-03-20 03:23:54 +0300

    Styled the Error Page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,28 @@
+import "./ErrorPage.css";
+import { TbError404 } from "react-icons/tb";
+import NavlinkButton from "../../components/NavlinkButton/NavlinkButton";
+
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <>
+      <div className="error-container">
+        <div className="page-title ">
+          <h1>Error Page 404</h1>
+        </div>
+        <div className="page-content">
+          <TbError404 color="#fff" size={300} />
+        </div>
+
+        <div className="page-subtitle mb-4 fz28  poppins-light">
+          Looks Like You are Lost?
+        </div>
+
+        <div className="page-cta">
+          <NavlinkButton toLink="/">Go To Home</NavlinkButton>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ErrorPage;
